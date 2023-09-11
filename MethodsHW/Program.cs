806c3bf81e18c39ace1a0da653b666bf22d4214b
Zorian Danylevych <zoriandanylevych@gmail.com>
2023-09-11 21:06:23 +0200

    I just wanna rock

--- MethodsHW/Program.cs
+++ MethodsHW/Program.cs
@@ -5,24 +5,26 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("This is the program that prints the maximal one from entered numbers, choose 'em! (Example: 1, 2, 3, 4, 5, 6, 7)");
+        Console.WriteLine("This is the program that prints the maximal and minimal numbers from entered numbers, choose 'em! (Example: 1, 2, 3, 4, 5, 6, 7)");
         string userInput = Console.ReadLine();
-        int maxNumber = Max(userInput);
-        Console.WriteLine("Max number is " + maxNumber);
+        FindMinMax(userInput);
     }
 
-    static int Max(string userInput)
+    static void FindMinMax(string userInput)
     {
         string[] nums = userInput.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
 
         if (nums.Length == 0)
         {
-            throw new ArgumentException("No valid numbers found in the input.");
+            throw new ArgumentException("Yo u, type the numbers in the correct format");
         }
 
         int[] intArray = nums.Select(int.Parse).ToArray();
 
-        int maxone = intArray.Max();
-        return maxone;
+        int maxNumber = intArray.Max();
+        int minNumber = intArray.Min();
+
+        Console.WriteLine("Max number is " + maxNumber);
+        Console.WriteLine("Min number is " + minNumber);
     }
 }
