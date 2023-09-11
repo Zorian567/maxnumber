4fed0c3479c003927c0e1f533c210ad9b723b183
Zorian Danylevych <zoriandanylevych@gmail.com>
2023-09-11 20:54:35 +0200

    Add project files.

---
+++ MethodsHW/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("This is the program that prints the maximal one from entered numbers, choose 'em! (Example: 1, 2, 3, 4, 5, 6, 7)");
+        string userInput = Console.ReadLine();
+        int maxNumber = Max(userInput);
+        Console.WriteLine("Max number is " + maxNumber);
+    }
+
+    static int Max(string userInput)
+    {
+        string[] nums = userInput.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (nums.Length == 0)
+        {
+            throw new ArgumentException("No valid numbers found in the input.");
+        }
+
+        int[] intArray = nums.Select(int.Parse).ToArray();
+
+        int maxone = intArray.Max();
+        return maxone;
+    }
+}
